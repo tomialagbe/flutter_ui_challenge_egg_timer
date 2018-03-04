aa7549b4a0ce819b15bd9cb0dc071109e178a154
Matt Carroll <me@mattcarroll.name>
2018-03-03 22:00:56 -0800

    Replaced bottom block with pause/resume button UI.

--- lib/main.dart
+++ lib/main.dart
@@ -57,9 +57,33 @@ class MyApp extends StatelessWidget {
                       ),
                     ],
                   ),
-                  new RandomColorBlock(
-                    width: double.INFINITY,
-                    height: 75.0,
+                  new FlatButton(
+                    splashColor: const Color(0x22000000),
+                    onPressed: () { },
+                    child: new Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          new Padding(
+                            padding: const EdgeInsets.only(right: 3.0),
+                            child: new Icon(
+                              Icons.pause,
+                              color: Colors.black,
+                            ),
+                          ),
+                          new Text(
+                            'Pause',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 3.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
