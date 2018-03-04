8d983e26ecf5db0bf4ecf37a56b2f32fe016c7c4
Matt Carroll <me@mattcarroll.name>
2018-03-03 23:13:25 -0800

    Added arrow under the knob circle that's painted with a custom painter.

--- lib/egg_timer_dial.dart
+++ lib/egg_timer_dial.dart
@@ -27,55 +27,103 @@ class EggTimerDial extends StatelessWidget {
             ],
           ),
           child: new Padding(
-            padding: const EdgeInsets.all(60.0),
-            child: new Container(
-                decoration: new BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: new LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
+            padding: const EdgeInsets.all(50.0),
+            child: new Stack(
+              children: [
+                new Container(
+                  width: double.INFINITY,
+                  height: double.INFINITY,
+                  child: new CustomPaint(
+                    painter: new ArrowPainter(),
                   ),
-                  boxShadow: [
-                    const BoxShadow(
-                      color: const Color(0x33000000),
-                      blurRadius: 4.0,
-                      spreadRadius: 1.0,
-                      offset: const Offset(0.0, 2.0),
-                    ),
-                    const BoxShadow(
-                      color: const Color(0x22000000),
-                      blurRadius: 6.0,
-                      spreadRadius: 3.0,
-                      offset: const Offset(0.0, 5.0),
-                    ),
-                  ],
                 ),
-                child: new Padding(
+                new Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: new Container(
                     decoration: new BoxDecoration(
                       shape: BoxShape.circle,
-                      color: Colors.transparent,
-                      border: new Border.all(
-                        color: const Color(0xFFDFDFDF),
-                        width: 1.5,
+                      gradient: new LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
                       ),
+                      boxShadow: [
+                        const BoxShadow(
+                          color: const Color(0x33000000),
+                          blurRadius: 4.0,
+                          spreadRadius: 1.0,
+                          offset: const Offset(0.0, 2.0),
+                        ),
+                        const BoxShadow(
+                          color: const Color(0x22000000),
+                          blurRadius: 6.0,
+                          spreadRadius: 3.0,
+                          offset: const Offset(0.0, 5.0),
+                        ),
+                      ],
                     ),
-                    child: new Center(
-                      child: new Image.network(
-                        'https://avatars3.githubusercontent.com/u/14101776?s=400&v=4',
-                        width: 50.0,
-                        height: 50.0,
-                        color: Colors.black,
+                    child: new Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.transparent,
+                          border: new Border.all(
+                            color: const Color(0xFFDFDFDF),
+                            width: 1.5,
+                          ),
+                        ),
+                        child: new Center(
+                          child: new Image.network(
+                            'https://avatars3.githubusercontent.com/u/14101776?s=400&v=4',
+                            width: 50.0,
+                            height: 50.0,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
-                    ),
+                    )
                   ),
-                )
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
   }
+}
+
+class ArrowPainter extends CustomPainter {
+
+  final dialArrowPaint;
+
+  ArrowPainter() : dialArrowPaint = new Paint() {
+    dialArrowPaint.color = Colors.black;
+    dialArrowPaint.strokeWidth = 3.0;
+    dialArrowPaint.style = PaintingStyle.fill;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.save();
+    canvas.translate(size.width / 2, 0.0);
+
+    Path path = new Path();
+    path.moveTo(0.0, -5.0);
+    path.lineTo(10.0, 10.0);
+    path.lineTo(-10.0, 10.0);
+    path.close();
+
+    canvas.drawShadow(path, Colors.black, 3.0, false);
+    canvas.drawPath(path, dialArrowPaint);
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
