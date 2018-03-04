6a56ac942814f5caeb7477f52b29b2ddd93c5107
Matt Carroll <me@mattcarroll.name>
2018-03-03 22:15:48 -0800

    Replaced dial block with 3 circles on top of each other.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:egg_timer/egg_timer_time_display.dart';
 import 'package:egg_timer/framing.dart';
 import 'package:flutter/material.dart';
 
+final GRADIENT_TOP = const Color(0xFFF5F5F5);
+final GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
+
 void main() {
   runApp(new MyApp());
 }
@@ -31,8 +34,41 @@ class MyApp extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 45.0, right: 45.0),
                 child: new AspectRatio(
                   aspectRatio: 1.0,
-                  child: new RandomColorBlock(
-                    width: double.INFINITY,
+                  child: new Container(
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: new LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
+                      ),
+                    ),
+                    child: new Padding(
+                      padding: const EdgeInsets.all(60.0),
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: new LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [GRADIENT_TOP, Colors.green],
+                          ),
+                        ),
+                        child: new Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: new Container(
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: new LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
+                              ),
+                            ),
+                          ),
+                        )
+                      ),
+                    ),
                   ),
                 ),
               ),
