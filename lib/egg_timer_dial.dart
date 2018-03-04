7f82be6091dcf7f3afc955d32e5cd3ec78080b55
Matt Carroll <me@mattcarroll.name>
2018-03-03 23:15:10 -0800

    Extracted knob into its own Knob class within the egg_timer_dial.dart file

--- lib/egg_timer_dial.dart
+++ lib/egg_timer_dial.dart
@@ -28,71 +28,79 @@ class EggTimerDial extends StatelessWidget {
           ),
           child: new Padding(
             padding: const EdgeInsets.all(50.0),
-            child: new Stack(
-              children: [
-                new Container(
-                  width: double.INFINITY,
-                  height: double.INFINITY,
-                  child: new CustomPaint(
-                    painter: new ArrowPainter(),
-                  ),
-                ),
-                new Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: new Container(
-                    decoration: new BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: new LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
-                      ),
-                      boxShadow: [
-                        const BoxShadow(
-                          color: const Color(0x33000000),
-                          blurRadius: 4.0,
-                          spreadRadius: 1.0,
-                          offset: const Offset(0.0, 2.0),
-                        ),
-                        const BoxShadow(
-                          color: const Color(0x22000000),
-                          blurRadius: 6.0,
-                          spreadRadius: 3.0,
-                          offset: const Offset(0.0, 5.0),
-                        ),
-                      ],
-                    ),
-                    child: new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new Container(
-                        decoration: new BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.transparent,
-                          border: new Border.all(
-                            color: const Color(0xFFDFDFDF),
-                            width: 1.5,
-                          ),
-                        ),
-                        child: new Center(
-                          child: new Image.network(
-                            'https://avatars3.githubusercontent.com/u/14101776?s=400&v=4',
-                            width: 50.0,
-                            height: 50.0,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    )
-                  ),
-                ),
-              ],
-            ),
+            child: new Knob(),
           ),
         ),
       ),
     );
   }
 }
+
+class Knob extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: [
+        new Container(
+          width: double.INFINITY,
+          height: double.INFINITY,
+          child: new CustomPaint(
+            painter: new ArrowPainter(),
+          ),
+        ),
+        new Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: new Container(
+              decoration: new BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: new LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
+                ),
+                boxShadow: [
+                  const BoxShadow(
+                    color: const Color(0x33000000),
+                    blurRadius: 4.0,
+                    spreadRadius: 1.0,
+                    offset: const Offset(0.0, 2.0),
+                  ),
+                  const BoxShadow(
+                    color: const Color(0x22000000),
+                    blurRadius: 6.0,
+                    spreadRadius: 3.0,
+                    offset: const Offset(0.0, 5.0),
+                  ),
+                ],
+              ),
+              child: new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new Container(
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.transparent,
+                    border: new Border.all(
+                      color: const Color(0xFFDFDFDF),
+                      width: 1.5,
+                    ),
+                  ),
+                  child: new Center(
+                    child: new Image.network(
+                      'https://avatars3.githubusercontent.com/u/14101776?s=400&v=4',
+                      width: 50.0,
+                      height: 50.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              )
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 
 class ArrowPainter extends CustomPainter {
 
