bd6a2e9a9a532c82ad22ac450d1f64ec8740689d
Matt Carroll <me@mattcarroll.name>
2018-03-06 21:10:07 -0800

    Replaced dial color blocks with 3 concentric circles with shadows and borders, and added icon to the center of the dial. Also extracted dial to egg_timer_dial.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:egg_timer/egg_timer_button.dart';
 import 'package:egg_timer/egg_timer_controls.dart';
+import 'package:egg_timer/egg_timer_dial.dart';
 import 'package:egg_timer/egg_timer_time_display.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttery/framing.dart';
+
+final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
+final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
 
 void main() {
   runApp(new MyApp());
@@ -18,29 +22,32 @@ class MyApp extends StatelessWidget {
         fontFamily: 'BebasNeue',
       ),
       home: new Scaffold(
-        body: new Center(
-          child: new Column(
-            children: [
-              new EggTimerTimeDisplay(
+        body: new Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [GRADIENT_TOP, GRADIENT_BOTTOM],
+            ),
+          ),
+          child: new Center(
+            child: new Column(
+              children: [
+                new EggTimerTimeDisplay(
 
-              ),
-              new RandomColorBlock(
-                width: double.INFINITY,
-                child: new Padding(
-                  padding: const EdgeInsets.only(left: 45.0, right: 45.0),
-                  child: new AspectRatio(
-                    aspectRatio: 1.0,
-                    child: new RandomColorBlock(
-                      width: double.INFINITY,
-                    ),
-                  ),
                 ),
-              ),
-              new Expanded(child: new Container()),
-              new EggTimerControls(
 
-              ),
-            ],
+                new EggTimerDial(
+
+                ),
+
+                new Expanded(child: new Container()),
+
+                new EggTimerControls(
+
+                ),
+              ],
+            ),
           ),
         ),
       ),
